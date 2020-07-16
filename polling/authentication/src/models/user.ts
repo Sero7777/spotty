82bac08d3e42b0374c374d7682b097f73d9ff207
Max Wagner <maxwagner@MacBook-Pro-2.local>
2020-07-16 16:16:46 +0200

    added userMoel

--- polling/authentication/src/models/user.ts
+++ polling/authentication/src/models/user.ts
@@ -1,52 +1,62 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 interface UserFields {
-    email: string
-    username: string
-    password: string
+  email: string;
+  username: string;
+  password: string;
 }
 
-interface UserModel extends mongoose.Model<UserDoc> {
-    build(fields: UserFields): UserDoc;
-  }
+interface UserModel extends mongoose.Model<UserDocument> {
+  build(fields: UserFields): UserDocument;
+}
 
-  interface UserDoc extends mongoose.Document {
-    email: string
-    username: string
-    password: string
-  }
+interface UserDocument extends mongoose.Document {
+  email: string;
+  username: string;
+  password: string;
+}
 
-  const userSchema = new mongoose.Schema(
-    {
-      email: {
-        type: String,
-        required: true
-      },
-      username: {
-          type: String,
-          required: true
-      },
-      password: {
-        type: String,
-        required: true
-      }
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
     },
-    {
-      toJSON: {
-        transform(doc, ret) {
-          ret.id = ret._id;
-          delete ret._id;
-          delete ret.password;
-          delete ret.__v;
-        }
-      }
-    }
-  );
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  }
+);
 
-  userSchema.statics.build = (fields: UserFields) => {
-    return new User(fields);
-  };
-  
-  const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
-  
-  export { User };
+userSchema.methods.toJSON = function() {
+    const user = this
+    const userObject = user.toObject()
+    delete userObject.password
+    delete userObject.__v
+    userObject.id = userObject.__id
+    delete userObject.__id
+}
+
+userSchema.statics.build = (fields: UserFields) => {
+  return new User(fields);
+};
+
+userSchema.statics.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+
+  next();
+});
+
+const User = mongoose.model<UserDocument, UserModel>("User", userSchema);
+
+export { User };
