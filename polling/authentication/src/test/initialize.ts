dcef16ca07d3345dd0b80a7266173b33b8d91f29
Max Wagner <maxwagner@MacBook-Pro-2.local>
2020-07-19 16:31:45 +0200

    trigger another gh actions run

--- polling/authentication/src/test/initialize.ts
+++ polling/authentication/src/test/initialize.ts
@@ -14,7 +14,7 @@ declare global {
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_SECRET = "Rabarbarkuchensalat1234";
+  process.env.JWT_SECRET = "Rabarbarkuchensalat12345";
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
