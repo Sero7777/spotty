5d621bb6319a48933eaed2ea05f466f203978846
Max Wagner <maxwagner@MacBook-Pro-2.local>
2020-07-23 18:31:50 +0200

    fixed a bug with react script making it impossible to run the dev serv in a docker container

--- polling/authentication/src/index.ts
+++ polling/authentication/src/index.ts
@@ -20,8 +20,8 @@ const initialize = async () => {
     console.error(err);
   }
 
-  app.listen(4000, () => {
-    console.log("Listening on port 4000 ...");
+  app.listen(3000, () => {
+    console.log("Listening on port 3000 ...");
   });
 };
 
