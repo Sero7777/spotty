1228d2dd808dc176327e3986166457e23a8f5164
Max Wagner <maxwagner@MacBook-Pro-2.local>
2020-07-26 17:38:41 +0200

    added routes to express

--- polling/comments/src/app.ts
+++ polling/comments/src/app.ts
@@ -6,12 +6,23 @@ import {
   RouteNotFoundException,
   errorHandler as exceptionHandler,
 } from "@spotty/shared";
+import {createSpotRouter} from "./routes/create"
+import {deleteCommentRouter} from "./routes/delete"
+import {updateCommentRouter} from "./routes/update"
+import {getSingleCommentRouter} from "./routes/getSingle"
+import {getAllCommentsBySpotsRouter} from "./routes/getAll"
+
 
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
 app.use(cookieSession({ signed: false, secure: false, httpOnly: false }));
-// routes
+
+app.use(createSpotRouter)
+app.use(deleteCommentRouter)
+app.use(updateCommentRouter)
+app.use(getSingleCommentRouter)
+app.use(getAllCommentsBySpotsRouter)
 
 app.all("*", async (req: Request, res: Response) => {
   throw new RouteNotFoundException();
