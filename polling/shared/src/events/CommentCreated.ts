12d9d704354ae96afc8fc2ff62750aef507ee242
Max Wagner <maxwagner@MacBook-Pro-2.local>
2020-07-30 15:34:44 +0200

    removed version property from commentCreatedEvent

--- polling/shared/src/events/CommentCreated.ts
+++ polling/shared/src/events/CommentCreated.ts
@@ -4,7 +4,6 @@ export interface CommentCreatedEvent {
   topic: Topics.CommentCreated;
   data: {
     id: string;
-    version: number;
     spot: string;
     content: string;
     username: string;
