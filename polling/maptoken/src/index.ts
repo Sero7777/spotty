d3a56558506ffc83b744cbb47cf97291e742ffe7
Max Wagner <maxwagner@MacBook-Pro-2.local>
2020-08-12 13:54:42 +0200

    added maptoken auth options

--- polling/maptoken/src/index.ts
+++ polling/maptoken/src/index.ts
@@ -2,19 +2,21 @@ import express, {Request, Response} from "express";
 import jwt from "jsonwebtoken"
 import { json } from "body-parser";
 import fs from "fs"
+import cors from "cors";
 
 const app = express()
 app.use(json());
+app.use(cors())
 
 app.get("/api/maptoken",async (req: Request, res: Response) => {
     const header = {
         "alg": "ES256",
         "typ": "JWT",
-        "kid": "mapIDkey"
+        "kid": "4HT6X57Z7A"
     }
 
     const payload = {
-        "iss": "teamID",
+        "iss": "935RVDZ54V",
         "iat": Date.now() / 1000,
         "exp": (Date.now() / 1000 + 1800)
     }
@@ -26,7 +28,8 @@ app.get("/api/maptoken",async (req: Request, res: Response) => {
         }
         else {
             const token = await jwt.sign(payload, data, {header: header})
-            res.send(token)
+            res.setHeader("Content-Type", "application/json")
+            res.send({token})
         }
     })
 })
