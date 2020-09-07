2904ae89b0d8df37def7f41d6c5223f1c3f13b31
Max Wagner <maxwagner@MacBook-Pro-2.local>
2020-09-07 14:57:55 +0200

    added sse changes

--- sse/query/src/routes/getAll.ts
+++ sse/query/src/routes/getAll.ts
@@ -5,18 +5,6 @@ import { auth } from "@spotty/shared";
 
 const getAllSpotsRouter = express.Router();
 
-let clients: any = [];
-
-const subscribe = (reqId: string, res: Response) => {
-  clients.push({ reqId, res });
-};
-
-const unsubscribe = (reqId: string) => {
-  clients = clients.filter((item: any) => item.reqId !== reqId);
-};
-
-const getClients = () => clients;
-
 getAllSpotsRouter.get(
   Uri.READALL,
   auth,
@@ -26,43 +14,4 @@ getAllSpotsRouter.get(
   }
 );
 
-getAllSpotsRouter.get(
-  Uri.CONNECT,
-  auth,
-  async (req: Request, res: Response) => {
-    const resHeaders = {
-      connection: "keep-alive",
-      "cache-control": "no-cache",
-      "content-Type": "text/event-stream",
-    };
-    res.set(resHeaders);
-
-    setInterval(() => {
-        res.write("data: \n\n")
-    },55000)
-
-    console.log(res.req!.headers["x-request-id"] + " connected")
-
-    if (
-      !res.req!.headers["x-request-id"] ||
-      Array.isArray(res.req!.headers["x-request-id"])
-    ) {
-      throw new Error();
-    }
-
-    subscribe(res.req!.headers["x-request-id"], res);
-
-    req.on("close", () => {
-      if (
-        !res.req!.headers["x-request-id"] ||
-        Array.isArray(res.req!.headers["x-request-id"])
-      ) {
-        throw new Error();
-      }
-      console.log(res.req!.headers["x-request-id"] + " Connection closed");
-      unsubscribe(res.req!.headers["x-request-id"]);
-    });
-  }
-);
-
-export { getAllSpotsRouter, getClients };
+export { getAllSpotsRouter };
--- sse/query/src/sse.ts
+++ sse/query/src/sse.ts
@@ -1,18 +1,27 @@
-import { getClients } from "./routes/getAll";
+import { Response } from "express";
+
+let clients: any = [];
+
+export const subscribe = (reqId: string, res: Response) => {
+  clients.push({ reqId, res });
+};
+
+export const unsubscribe = (reqId: string) => {
+  clients = clients.filter((item: any) => item.reqId !== reqId);
+};
 
 export const notifyClients = (type: string, payload: any) => {
-    var clients = getClients()
-    const item = {
-        type: type,
-        payload: payload
-    }
-    clients.forEach((element: any) => {
-        element.res.write("data: " + JSON.stringify(item) + "\n\n")
-    });
-}
+  const item = {
+    type: type,
+    payload: payload,
+  };
+  clients.forEach((element: any) => {
+    element.res.write("data: " + JSON.stringify(item) + "\n\n");
+  });
+};
 
 export enum actions {
-    ADD_SPOT = "ADD_SPOT",
-    DELETE_SPOT = "DELETE_SPOT",
-    UPDATE_SPOT = "UPDATE_SPOT",
+  ADD_SPOT = "ADD_SPOT",
+  DELETE_SPOT = "DELETE_SPOT",
+  UPDATE_SPOT = "UPDATE_SPOT",
 }
